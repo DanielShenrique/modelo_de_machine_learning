4fdefc251f5246d478b93f326b788857f4ce748b
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2023-09-09 19:57:11 -0300

    Feat: mensagem padrão de boas vindas

--- bot/detect_spam_bot.py
+++ bot/detect_spam_bot.py
@@ -5,4 +5,16 @@ CHAVE_API = config('API_KEY')
 
 bot = telebot.TeleBot(CHAVE_API)
 
+textoPadrao = """
+Ola Seja bem-vindo
+O bot de processamento de (nlp) linguagem natural.
+você poderar verificar e ter uma maior certeza do que e fraude ou não da sua mensagem."""
+
+def verificar(mensagem):
+    return True
+
+@bot.message_handler(commands=["start"])
+def boasVindas(mensagem):
+    bot.reply_to(mensagem,textoPadrao)
+
 bot.polling()
