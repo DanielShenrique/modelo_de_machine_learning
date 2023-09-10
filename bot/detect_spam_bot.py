ed3d868d7295be9eeee858de173755296b1938f1
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2023-09-10 11:54:01 -0300

    Feat: mensagem para apresentar os comandos

--- bot/detect_spam_bot.py
+++ bot/detect_spam_bot.py
@@ -5,13 +5,20 @@ CHAVE_API = config('API_KEY')
 
 bot = telebot.TeleBot(CHAVE_API)
 
+listaComandos = ["comandos","start","estatos"]
+
 textoPadrao = """
 Ola Seja bem-vindo
 O bot de processamento de (nlp) linguagem natural.
 você poderar verificar e ter uma maior certeza do que e fraude ou não da sua mensagem."""
 
-def verificar(mensagem):
-    return True
+@bot.message_handler(commands=["comandos","commands","c"])
+def status(mensagem):
+    textoComandos = f"temos {len(listaComandos)} commandos\n"
+
+    for comando in listaComandos:
+        textoComandos = f"{textoComandos}Comando /{comando}\n"
+    bot.send_message(mensagem.chat.id , textoComandos)
 
 @bot.message_handler(commands=["start"])
 def boasVindas(mensagem):
