5f742bada273973d719271a52f2718e359abf4d2
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2023-10-29 12:04:57 -0300

    Feat: puxando dataset

---
+++ grafico.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(filepath_or_buffer="data/Dataset_5971.csv", sep=",")
+
+df["LABEL"].replace("Spam", "spam", inplace=True)
+df["LABEL"].replace("Smishing", "smishing", inplace=True)
+
+df.drop(["URL", "EMAIL", "PHONE"], axis=1, inplace=True)
+
+df.drop_duplicates(inplace=True)
+
+df.reset_index(drop=True, inplace=True)
